1c105f4e355febfd86db46304d60cf977a9c8ed5
Michael Harrison <harrison.michael@bitsourceky.com>
2015-11-20 20:00:47 -0500

    trying to push

--- query.php
+++ query.php
@@ -1,4 +1,7 @@
 <link href="css/css/bootstrap.min.css" rel="stylesheet">
+<link href="css/css/override.css" rel="stylesheet">
+<link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+
 <?php
 require_once('config.php');
 
@@ -13,72 +16,72 @@ $imdbid = $response['imdbID'];
 
 <html>
 <form action="addmovie.php" method="post">
-  <div class="container col-md-4 col-md-offset-4">
+  <div class="container col-md-4 col-md-offset-4 btn-padding-remove">
     <button name="sendtocp" value="<?php echo $imdbid ?>" class="btn btn-primary" style="margin-top: 5px; padding: 20px 5px; width: 100%">Add Movie!</button>
   </div>
 </form>
-  <div class="container col-md-4 col-md-offset-4" style="padding: 10px 10px">
-  <table class="table table-striped">
-    <tr>
-     <th>Poster</th>
-    </tr>
-    <tr>
-      <td align="center"><img src="<?php echo $response['Poster']; ?>"</td>
-    </tr>
-    <tr>
-     <th>Movie Title</th>
-    </tr>
-    <tr>
-      <td align="center"><?php echo $response['Title']; ?></td>
-    </tr>
-    <tr>
-     <th>Release Date</th>
-    </tr>
-    <tr>
-      <td align="center"><?php echo $response['Released']; ?></td>
-    </tr>
-    <tr>
-     <th>MPAA Rating</th>
-    </tr>
-    <tr>
-      <td align="center"><?php echo $response['Rated']; ?></td>
-    </tr>
-    <tr>
-     <th>Runtime</th>
-    </tr>
-    <tr>
-      <td align="center"><?php echo $response['Runtime']; ?></td>
-    </tr>
-    <tr>
-     <th>Genre</th>
-    </tr>
-    <tr>
-      <td align="center"><?php echo $response['Genre']; ?></td>
-    </tr>
-    <tr>
-     <th>Cast</th>
-    </tr>
-    <tr>
-      <td align="center"><?php echo $response['Actors']; ?></td>
-    </tr>
-    <tr>
-     <th>Plot</th>
-    </tr>
-    <tr>
-      <td align="center"><?php echo $response['Plot']; ?></td>
-    </tr>
-    <tr>
-     <th>IMDb Rating</th>
-    </tr>
-    <tr>
-      <td align="center"><?php echo $response['imdbRating']; ?></td>
-    </tr>
-    <tr>
-     <th>IMDb ID</th>
-    </tr>
-    <tr>
-      <td align="center"><?php echo $response['imdbID']; ?></td>
-    </tr>
-  </table>
-</div>
+  <body>
+    <div class="container col-md-4 col-md-offset-4 well" style="padding: 10px 10px">
+       <div class="well">Poster <hr>
+         <div class="text-center">
+           <img src="<?php echo $response['Poster']; ?>" >
+         </div>
+       </div>
+       
+       <div class="well">Movie Title <hr>
+         <div class="text-center">
+          <?php echo $response['Title']; ?>
+        </div>
+       </div>
+       
+       <div class="well">Release Date <hr>
+         <div class="text-center">
+          <?php echo $response['Released']; ?>
+        </div>
+       </div>
+
+       <div class="well">MPAA Rating <hr>
+         <div class="text-center">
+          <?php echo $response['Rated']; ?>
+        </div>
+       </div>
+
+       <div class="well">Runtime <hr>
+         <div class="text-center">
+          <?php echo $response['Runtime']; ?>
+        </div>
+       </div>
+
+       <div class="well">Genre <hr>
+         <div class="text-center">
+          <?php echo $response['Genre']; ?>
+        </div>
+       </div>
+
+       <div class="well">Cast <hr>
+         <div class="text-center">
+          <?php echo $response['Actors']; ?>
+        </div>
+       </div>
+
+       <div class="well">Plot <hr>
+         <div class="text-center">
+          <?php echo $response['Plot']; ?>
+        </div>
+       </div>
+
+       <div class="well">IMDb Rating <hr>
+         <div class="text-center">
+          <?php echo $response['imdbRating']; ?>
+        </div>
+       </div>
+
+       <div class="well">IMDb ID <hr>
+         <div class="text-center">
+          <?php echo $response['imdbID']; ?>
+        </div>
+       </div>
+
+    </div>
+  </body>
 </html>
